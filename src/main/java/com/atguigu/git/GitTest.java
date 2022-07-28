2ce92abf6ce10cbfcbcc8ab7c8631744d9c16634
Alex <550344120@qq.com>
2022-07-28 15:38:45 +0800

    hot-fix commit1

--- src/main/java/com/atguigu/git/GitTest.java
+++ src/main/java/com/atguigu/git/GitTest.java
@@ -3,9 +3,10 @@ package com.atguigu.git;
 public class GitTest {
 
     public static void main(String[] args) {
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
-        System.out.println("hello git");
+        System.out.println("hello git 1");
+        System.out.println("hello git 2");
+        System.out.println("hello git 3");
+        System.out.println("hello git 4");
+        System.out.println("hello git 5");
     }
 }
